e217319c24091d98aba3e54bb7de61ceaf6544d3
angrha <angrha@gmail.com>
2017-10-27 21:51:37 +0700

    first commit release 0, aang

---
+++ setup.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('database.db');
+
+db.serialize( () => {
+  db.run(`CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(20),
+            company VARCHAR(30),
+            telp VARCHAR(15),
+            email VARCHAR(30))`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS groups(id INTEGER, name_of_group VARCHAR(25))`);
+  db.run(`CREATE TABLE IF NOT EXISTS profiles(id INTEGER, username VARCHAR(15), password VARCHAR(20))`);
+  db.run(`CREATE TABLE IF NOT EXISTS addresses(id INTEGER, street VARCHAR(80), city VARCHAR(20), zipcode INTEGER)`);
+});
+  db.close();

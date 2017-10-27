c5eb214f60ee1cd0df74a1880654fc71549b3078
Rizky <co.terbang@gmail.com>
2017-10-27 15:29:10 +0700

    setup.js done

---
+++ setup.js
@@ -0,0 +1,27 @@
+const sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('./database/database.db');
+ 
+db.serialize( () =>
+  {
+    db.run(`CREATE TABLE IF NOT EXISTS Contacts (
+              ID INTEGER PRIMARY KEY AUTOINCREMENT,
+              name varchar(25),
+              company varchar(25),
+              telp_number varchar(25),
+              email varchar(50))`);
+    db.run(`CREATE TABLE IF NOT EXISTS Groups (
+              ID INTEGER PRIMARY KEY AUTOINCREMENT,
+              name_of_group varchar(25))`);
+    db.run(`CREATE TABLE IF NOT EXISTS Profile (
+              ID INTEGER PRIMARY KEY AUTOINCREMENT,
+              username varchar(50),
+              password varchar(50))`);
+    db.run(`CREATE TABLE IF NOT EXISTS Addresses (
+              ID INTEGER PRIMARY KEY AUTOINCREMENT,
+              street varchar (100),
+              city varchar(50),
+              zipcode varchar(6))`);
+  }
+);
+ 
+db.close();

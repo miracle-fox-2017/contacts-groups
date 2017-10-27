26ef6a48558b0c2fb53c30f5b7335b27e3f16fdd
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-10-27 23:04:59 +0700

    Release 1 - Input Group

---
+++ app.js
@@ -0,0 +1,101 @@
+const parser=require("body-parser");
+const express=require("express");
+const sqlite3=require("sqlite3");
+
+// Instance
+const app=express();
+const db=new sqlite3.Database("./database/database.db");
+
+// Body Parser
+app.use(parser.urlencoded({extended:false}));
+app.use(parser.json());
+
+// EJS
+app.set("views","./public") // Public directory
+app.set("view engine","ejs"); // Template engine
+app.use(express.static("./public")); // Static file
+
+//======================> Ladies & Gentlemen, let's get started!
+// Index Page / Main Page
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+// Contact
+app.get("/contacts",(req,res)=>{ // Halaman awal kontak
+    db.all(`SELECT * FROM contacts`,(err,data)=>{
+        if(err){
+            throw err;
+        }else{
+            res.render("contact",{data:data});
+        }
+    });
+});
+app.post("/contacts",(req,res)=>{ // Tambah kontak
+    const data=req.body;
+    db.run(
+        `INSERT INTO contacts
+        (name, company, phone, email)
+        VALUES ("${data.name}", "${data.company}", "${data.phone}", "${data.email}")`
+    );
+    db.redirect("/contacts");
+});
+app.get("/contacts/edit/:id",(req,res)=>{ // Halaman Edit Kontak
+    db.all(`SELECT * FROM contacts WHERE id="${req.params.id}"`,(err,data)=>{
+        if(err){
+            throw err;
+        }else if(data.length === 0){
+            res.redirect("/contacts");
+        }else{
+            res.render("edit-contact",{data:data});
+        }
+    });
+});
+app.post("/contacts/edit/:id",(req,res)=>{ // Edit Kontak
+    const data=req.body;
+    db.run(
+        `UPDATE contacts SET
+        name="${data.name}",
+        company="${data.company}",
+        phone="${data.phone}",
+        email="${data.email}"
+        WHERE id="${data.id}"`
+    );
+    res.redirect("/contacts");
+});
+app.get("/contacts/delete/:id",(req,res)=>{ // Hapus Kontak
+    db.all(`SELECT * FROM contacts WHERE id="${req.params.id}"`,(err,data)=>{
+        if(data.length === 0){
+            res.redirect("/contacts");
+        }else{
+            db.run(
+                `DELETE FROM contacts
+                WHERE id="${req.params.id}"`
+            );
+            res.redirect("/contacts");
+        }
+    });
+});
+app.get("/groups",(req,res)=>{ // Halaman awal group
+    db.all(`SELECT * FROM groups`,(err,data)=>{
+        if(err){
+            throw err;
+        }else{
+            res.render("groups",{data:data});
+        }
+    });
+});
+app.post("/groups",(req,res)=>{ // Tambah group
+    const data=req.body;
+    db.run(
+        `INSERT INTO groups (name_of_group)
+        VALUES ("${data.group}")`
+    );
+    res.redirect("/groups");
+});
+
+// Listen Port
+app.listen(3000,()=>{
+    console.log("Server started!");
+    console.log("Listening on port 3000");
+});
--- setup.js
+++ setup.js
@@ -1,18 +1,36 @@
-const sqlite3 = require("sqlite3");
-const db = new sqlite3.Database("./database/database.db");
+const sqlite3=require("sqlite3");
+const db=new sqlite3.Database("./database/database.db");
 
 db.serialize(()=>{
     db.run(
-        "CREATE TABLE IF NOT EXISTS contacts(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, company TEXT, phone VARCHAR, email VARCHAR)"
+        `CREATE TABLE IF NOT EXISTS contacts(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            company TEXT NOT NULL,
+            phone VARCHAR NOT NULL,
+            email VARCHAR NOT NULL
+        )`
     );
     db.run(
-        "CREATE TABLE IF NOT EXISTS groups(id INTEGER PRIMARY KEY AUTOINCREMENT, name_of_group VARCHAR)"
+        `CREATE TABLE IF NOT EXISTS groups(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name_of_group VARCHAR NOT NULL
+        )`
     );
     db.run(
-        "CREATE TABLE IF NOT EXISTS profile(id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR, password VARCHAR)"
+        `CREATE TABLE IF NOT EXISTS profile(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username VARCHAR NOT NULL,
+            password VARCHAR NOT NULL
+        )`
     );
     db.run(
-        "CREATE TABLE IF NOT EXISTS address(id INTEGER PRIMARY KEY AUTOINCREMENT, street VARCHAR, city TEXT, zipcode INTEGER)"
+        `CREATE TABLE IF NOT EXISTS address(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            street VARCHAR NOT NULL,
+            city TEXT NOT NULL,
+            zipcode INTEGER NOT NULL
+        )`
     );
 });
 db.close();

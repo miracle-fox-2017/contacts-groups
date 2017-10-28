22629fe5b2e770c6dde58c31d0c4ccaeaf12413b
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-28 14:03:57 +0700

    backup

--- setup.js
+++ setup.js
@@ -9,6 +9,12 @@ db.serialize(() => {
 	db.run('CREATE TABLE IF NOT EXISTS Profile (id INTEGER PRIMARY KEY AUTOINCREMENT,  username VARCHAR(25), password VARCHAR(25))');
 
 	db.run('CREATE TABLE IF NOT EXISTS Addresses (id INTEGER PRIMARY KEY AUTOINCREMENT,  street VARCHAR(25), city VARCHAR(25), zipcode INTEGER)');
+
+	// Modif dari original
+	db.run(`ALTER TABLE Profile ADD contacts_id INTEGER FOREIGNKEY`, (err) => {
+		console.log(err);
+	});
+	// db.run(`ALTER TABLE Profile ADD CONSTRAINT contacts_id UNIQUE (contacts_id)`);
 });
 
 db.close();

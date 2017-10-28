95f9f4edba3973362728f9476b0baea9c736d1d8
vbagustinus <vbagustinus@gmail.com>
2017-10-29 04:20:04 +0700

    Release 9

--- app.js
+++ app.js
@@ -23,25 +23,56 @@ app.get('/', (req, res)=>{
 
 // CONTACTS
 app.get('/contacts', (req, res)=>{
-  let query = `SELECT * FROM Contacts`
-  db.all(query, (err, rows)=>{
+  // db.all(`SELECT * FROM Addresses` ,(err, rowAddress)=>{
+  //       var count = 0
+  //       rowAddress.forEach((row) => {
+  //         //get contact by row.contact_id
+  //         Contact.getIdContacts(row.ContactsId)
+  //         .then((dataContact) => {
+  //           row.contactName = dataContact.nama
+  //           row.company = dataContact.company
+  //           count++
+  //           if(count == rowAddress.length) {
+  //             resolve(rowAddress)
+  //           }
+  //         })
+  //       })
+  //     });
+  //contacts -->Contacts_Groups --> groups
+  let msgError;
+  if(req.query.hasOwnProperty('msgError')){
+    msgError = "Email sudah terdaftar, coba email lain"
+  }
+  let queryGroups = `SELECT * FROM Groups`
+  db.all(queryGroups, (err, dataGroups)=>{
     if(!err){
-      let queryGroups = `SELECT * FROM Groups`
-      db.all(queryGroups, (err, dataGroups)=>{
+                                                                                            // `SELECT detail.id,detail.user_id, user.name, detail.movie_id,title,price FROM detail INNER JOIN user ON detail.user_id = user.id INNER JOIN movie ON detail.movie_id=movie.id;`
+      let query = `SELECT Contacts.id, Contacts.name, Contacts.company, Contacts_Groups.GroupsId, Contacts_Groups.ContactsId, Contacts.telp_number, Contacts.email, Groups.name_of_group FROM Contacts_Groups JOIN Contacts ON Contacts_Groups.ContactsId = Contacts.id   JOIN Groups ON Contacts_Groups.GroupsId = Groups.id`
+      db.all(query, (err, rows)=>{
         if(!err){
-          res.render('contacts', {rowsContacts:rows, dataGroups:dataGroups})
+          db.all(`SELECT * FROM Contacts_Groups`, (err, dataConjuction)=>{
+            if(!err){
+              res.send(rows)
+                res.render('contacts', {msgError:msgError,rowsContacts:rows, dataGroups:dataGroups,dataConjuction:dataConjuction})
+            } else {
+              res.send(err)
+            }
+          })
+
         } else {
           res.send(err)
+          console.log(err);
         }
       })
     } else {
       res.send(err)
-      console.log(err);
     }
   })
+
 })
 
 app.post('/contacts', (req, res)=>{
+  // res.send(req)
   let query = `INSERT INTO Contacts (name, company, telp_number, email) VALUES ('${req.body.name}','${req.body.company}','${req.body.telp_number}','${req.body.email}')`
   db.run(query, (err)=>{
     if(!err){
@@ -54,7 +85,7 @@ app.post('/contacts', (req, res)=>{
                   if(!err){
                     res.redirect('/contacts')
                   } else {
-                    res.send(err)
+                    res.redirect('/contacts/?msgError=true')
                   }
                 })
               }
@@ -64,7 +95,7 @@ app.post('/contacts', (req, res)=>{
           }
         })
     } else {
-      res.send(err)
+      res.redirect('/contacts/?msgError=true')
     }
   })
 })
@@ -95,10 +126,14 @@ app.get('/contacts/delete/:id', (req, res)=>{
 
 // GROUPS
 app.get('/groups', (req, res)=>{
+  let msgError;
+  if(req.query.hasOwnProperty('msgError')){
+    msgError = "Nama grup sudah ada, coba nama lainya"
+  }
   let query = `SELECT * FROM Groups`
   db.all(query, (err, rows)=>{
     if(!err){
-      res.render('groups', {rowsGroups:rows})
+      res.render('groups', {msgError:msgError,rowsGroups:rows})
     } else {
       res.send(err)
       console.log(err);
@@ -108,8 +143,14 @@ app.get('/groups', (req, res)=>{
 
 app.post('/groups', (req, res)=>{
   let query = `INSERT INTO Groups (name_of_group) VALUES ('${req.body.name_of_group}')`
-  db.run(query)
-  res.redirect('/groups')
+  db.run(query, (err)=>{
+    if(!err){
+      res.redirect('/groups')
+    } else {
+      res.redirect('/groups/?msgError=true')
+    }
+  })
+
 })
 
 app.get('/groups/edit/:id', (req, res)=>{
@@ -138,9 +179,10 @@ app.get('/groups/delete/:id', (req, res)=>{
 
 // ADDRESSES
 app.get('/addresses', (req, res)=>{
-  let query = `SELECT Addresses.id, Addresses.street, Addresses.city, Addresses.zipcode, Contacts.name FROM Addresses LEFT JOIN Contacts ON Addresses.ContactsId = Contacts.id`
+  let query = `SELECT Addresses.id, Addresses.street, Addresses.city, Addresses.zipcode,Addresses.ContactsId, Contacts.name FROM Addresses LEFT JOIN Contacts ON Addresses.ContactsId = Contacts.id`
   db.all(query, (err, rows)=>{
     if(!err){
+      // res.send(rows)
       let queryContacts = `SELECT * FROM Contacts`
       db.all(queryContacts, (err, dataContacts)=>{
         if(!err){
@@ -204,28 +246,33 @@ app.get('/addresses/delete/:id', (req, res)=>{
 
 // PROFILES
 app.get('/profiles', (req, res)=>{
+  let msgError;
+  if(req.query.hasOwnProperty('msgError')){
+    msgError = "Username sudah ada coba yang lainya"
+  }
   let query = `SELECT Profile.id, Profile.username, Profile.password, Contacts.name FROM Profile LEFT JOIN Contacts ON Profile.ContactsId = Contacts.id`
   db.all(query, (err, rows)=>{
     if(!err){
       let queryContacts = `SELECT * FROM Contacts`
       db.all(queryContacts, (err, dataContacts)=>{
-          res.render('profiles', {rowsProfiles:rows, dataContacts:dataContacts})
+          res.render('profiles', {msgError:msgError,rowsProfiles:rows, dataContacts:dataContacts})
           // res.send(dataContacts)
       })
     } else {
-      res.render('profiles', {error :''})
+      res.render('profiles')
       console.log(err);
     }
   })
 })
 
 app.post('/profiles', (req, res)=>{
+
   let query = `INSERT INTO Profile (username, password, ContactsId) VALUES ('${req.body.username}','${req.body.password}', '${req.body.ContactsId}')`
   db.run(query, (err)=>{
     if(!err){
       res.redirect('/profiles')
     } else  {
-      res.render('profiles',{error :''})
+      res.redirect('/profiles/?msgError=true')
       // res.send(err)
       // console.log(err);
     }
@@ -272,7 +319,7 @@ app.get('/profiles/delete/:id', (req, res)=>{
 })
 
 //ADDRESSES WITH CONTACTS
-app.get('/addresses_with_contact', (req, res)=>{
+app.get('/addresses_with_contact', (req, res) => {
   let query = `SELECT Addresses.id, Addresses.street, Addresses.city, Addresses.zipcode, Contacts.name, Contacts.company FROM Addresses LEFT JOIN Contacts ON Addresses.ContactsId = Contacts.id`
   db.all(query, (err, rows)=>{
     if(!err){
@@ -284,10 +331,40 @@ app.get('/addresses_with_contact', (req, res)=>{
   })
 })
 
+//ASSIG GROUPS
+
+app.get('/groups/assign_contacts/:id_group', (req, res)=>{
+  let query = `SELECT * FROM Groups WHERE id = '${req.params.id_group}'`
+  db.get(query, (err, dataGroup)=>{
+    if(!err){
+      let queryContacts = `SELECT * FROM Contacts`
+      db.all(queryContacts, (err, dataContacts)=>{
+        if(!err){
+          res.render('assignContacts', {dataGroup:dataGroup, dataContacts:dataContacts})
+        } else {
+          res.send(err)
+        }
+      })
+    } else {
+      res.send(err)
+    }
+  })
+})
+
+app.post('/groups/assign_contacts/:id_group', (req, res)=>{
+  let query = `INSERT INTO Contacts_Groups (ContactsId, GroupsId) VALUES ('${req.body.ContactsId}', '${req.params.id_group}')`
+  db.run(query,(err)=>{
+    if(!err){
+      res.redirect('/groups')
+    } else {
+      res.send(err)
+    }
+  })
+})
 //RUNNING APP SERVER
 app.listen(3000,(err)=>{
   if(!err){
-    console.log('Running ata 3000');
+    console.log('OTW di pelabuhan 3000');
   } else {
     console.log(err);
   }

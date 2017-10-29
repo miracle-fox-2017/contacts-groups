a29e480b970d100f66ac891f07f273fbcbc3eb15
riskinputra <riski.n.putra@gmail.com>
2017-10-29 08:29:16 +0700

    release-2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/', function(req, res) {
 })
 
 // ----- CONTACTS -----
-// SHOW
+// SELECT
 app.get('/contacts', function (req, res) {
   let query = `SELECT * FROM contacts`;
   db.all(query, (err, rows)=>{
@@ -79,7 +79,7 @@ app.get('/contacts/delete/:id', (req, res)=>{
 })
 
 
-// GROUPS
+// ----------- GROUPS --------------
 // SELECT
 app.get('/groups', (req, res)=>{
   let query = `SELECT * FROM groups`;
@@ -104,7 +104,7 @@ app.post('/groups', (req, res)=>{
 
 // UPDATE
 app.get('/groups/edit/:id', (req, res)=>{
-  let query = `SELECT * FROM groups WHERE id = '${req.param.id}'`
+  let query = `SELECT * FROM groups WHERE id = '${req.params.id}'`
 
   db.all(query, (err, rows)=>{
     res.render('groups-edit', {rows}) // groups-edit adalah nama_file
@@ -130,6 +130,130 @@ app.get('/groups/delete/:id', (req, res)=>{
     res.redirect('/groups')
   })
 })
+
+// -------- ADDRESS ----------
+// SELECT
+app.get('/address', (req, res)=>{
+  let query = `SELECT * FROM address`;
+
+  db.all(query, (err, rows)=>{
+    res.render('address', {rows})
+  })
+})
+
+// INSERT
+app.post('/address', (req, res)=>{
+  let street = req.body.street;
+  let city = req.body.city;
+  let zipcode = req.body.zipcode;
+
+  let query = `INSERT INTO address
+              (street, city, zipcode)
+              VALUES
+              ('${street}', '${city}' , '${zipcode}') `;
+
+  db.run(query, ()=>{
+    res.redirect('/address');
+  })
+
+})
+
+// UPDATE
+app.get('/address/edit/:id', (req, res)=>{
+  let query = `SELECT * FROM address WHERE id = '${req.params.id}'`
+
+  db.all(query, (err, rows)=>{
+    res.render('address-edit', {rows})
+  })
+})
+
+app.post('/address/edit/:id', (req, res)=>{
+  let id = req.body.id;
+  let street = req.body.street;
+  let city = req.body.city;
+  let zipcode = req.body.zipcode;
+
+
+  let query = `UPDATE address SET street = '${street}', city = '${city}', zipcode = '${zipcode}' WHERE id = '${id}'`;
+
+  db.run(query, ()=>{
+    res.redirect('/address');
+  });
+
+})
+
+// DELETE
+app.get('/address/delete/:id', (req, res)=>{
+  let id = req.params.id;
+  let query = `DELETE FROM address WHERE id = '${id}'`
+
+  db.run(query,()=>{
+    res.redirect('/address')
+  })
+})
+
+// --------PROFILE-------
+// SELECT
+app.get('/profiles', (req, res)=>{
+  let query = `SELECT * FROM profile`;
+
+  db.all(query, (err, rows)=>{
+    res.render('profile', {rows})
+  })
+})
+
+// INSERT
+app.post('/profiles', (req, res)=>{
+  let username = req.body.username;
+  let password = req.body.password;
+  let query = `INSERT INTO
+              profile (username, password)
+              VALUES
+              ('${username}', '${password}')`;
+
+
+  db.run(query, ()=>{
+    res.redirect('/profiles');
+  })
+
+})
+
+// UPDATE
+app.get('/profiles/edit/:id', (req, res)=>{
+  let id = req.params.id;
+  let query = `SELECT * FROM profile WHERE id = '${id}'`;
+
+  db.all(query, (err, rows)=>{
+    res.render('profile-edit', {rows});
+  })
+})
+
+app.post('/profiles/edit/:id', (req, res)=>{
+  let id = req.body.id;
+  let username = req.body.username;
+  let password = req.body.password;
+  let query = `UPDATE profile
+              SET
+              username = '${username}', password = '${password}'
+              WHERE
+              id = '${id}'`;
+
+  db.run(query, ()=>{
+    res.redirect('/profiles');
+  })
+
+  // DELETE
+  app.get('/profiles/delete/:id', (req, res)=>{
+    let id = req.params.id;
+    let query = `DELETE FROM profile WHERE id = '${id}'`
+
+    db.run(query, ()=>{
+      res.redirect('/profiles')
+    })
+  })
+
+})
+
 
 // LOCALHOST CONNECTION
 app.listen(3000, function() {

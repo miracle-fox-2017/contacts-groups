7c8c34e618ad0f7b06be8d0fc955bc6af57261b8
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-10-29 18:37:52 +0700

    Release 3 - Done

--- setup.js
+++ setup.js
@@ -2,34 +2,44 @@ const sqlite3=require("sqlite3");
 const db=new sqlite3.Database("./database/database.db");
 
 db.serialize(()=>{
-    db.run(
+    db.run( // Contact Table
         `CREATE TABLE IF NOT EXISTS contacts(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INT PRIMARY KEY AUTOINCREMENT,
             name TEXT,
             company TEXT,
             phone VARCHAR,
             email VARCHAR
         )`
     );
-    db.run(
+    db.run( // Group Table
         `CREATE TABLE IF NOT EXISTS groups(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INT PRIMARY KEY AUTOINCREMENT,
             name_of_group VARCHAR
         )`
     );
-    db.run(
+    db.run( // Profile Table
         `CREATE TABLE IF NOT EXISTS profile(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INT PRIMARY KEY AUTOINCREMENT,
             username VARCHAR,
             password VARCHAR
-        )`
+        );
+        CREATE TABLE new_profile(
+            id INT PRIMARY KEY AUTOINCREMENT,
+            username VARCHAR,
+            password VARCHAR,
+            contact_id INT UNIQUE
+        );
+        INSERT INTO new_profile(id,username,password)
+        SELECT * FROM profile;
+        DROP TABLE profile;
+        ALTER TABLE new_profile RENAME TO profile;`
     );
-    db.run(
+    db.run( // Address Table
         `CREATE TABLE IF NOT EXISTS address(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id INT PRIMARY KEY AUTOINCREMENT,
             street VARCHAR,
             city TEXT,
-            zipcode INTEGER
+            zipcode INT
         )`
     );
 });

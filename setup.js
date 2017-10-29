17abc66cbc7f8b3317bc060e507ec6a337edbf4c
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-29 12:49:23 +0700

    Rilis 6

--- setup.js
+++ setup.js
@@ -10,7 +10,9 @@ db.serialize(() => {
 
 	db.run('CREATE TABLE IF NOT EXISTS Addresses (id INTEGER PRIMARY KEY AUTOINCREMENT,  street VARCHAR(25), city VARCHAR(25), zipcode INTEGER)');
 
-	// Modif dari original Profile untuk tambah contacts_id unique FK
+	// RILIS 3: Modif dari original Profile untuk tambah contacts_id unique FK
+	// Comment script ini tiap ganti table property dirilis berikutnya agar tidak dijalankan karena datanya sudah terbaru
+	// dan tidak perlu tambah kolom lagi
 	db.run(`CREATE TABLE IF NOT EXISTS Profile_temp (
 		id	INTEGER PRIMARY KEY AUTOINCREMENT,
 		username	VARCHAR(25),
@@ -22,6 +24,22 @@ db.serialize(() => {
 	db.run(`INSERT INTO Profile_temp (id, username, password) SELECT * FROM Profile`);
 	db.run(`DROP TABLE Profile`);
 	db.run(`ALTER TABLE Profile_temp RENAME TO Profile`);
+
+	// Rilis 6
+	// Comment script ini tiap ganti table property dirilis berikutnya agar tidak dijalankan karena datanya sudah terbaru
+	// dan tidak perlu tambah kolom lagi
+	db.run(`CREATE TABLE IF NOT EXISTS Addresses_temp (
+		id	INTEGER PRIMARY KEY AUTOINCREMENT,
+		street	VARCHAR(25),
+		city	VARCHAR(25),
+		zipcode	INTEGER,
+		contacts_id	INTEGER UNIQUE,
+		FOREIGN KEY (contacts_id) REFERENCES Contacts(id)
+	)`);
+
+	db.run(`INSERT INTO Addresses_temp (id, street, city, zipcode) SELECT * FROM Addresses`);
+	db.run(`DROP TABLE Addresses`);
+	db.run(`ALTER TABLE Addresses_temp RENAME TO Addresses`);
 });
 
 db.close();

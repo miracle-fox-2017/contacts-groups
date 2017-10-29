fa1aa0bb3e0de77f9d09ad81edaa83de9792d641
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-10-29 08:02:32 +0700

    contacts crud done

--- models/contacts.js
+++ models/contacts.js
@@ -1,30 +1,38 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('../data.db');
+const db = new sqlite3.Database('database/data.db');
 
 class Contact {
   static findAll(callback) {
-    db.all(`SELECT * FROM Contacts`, function(err, rows) {
-      // console.log(rows);
+    db.all(`SELECT * FROM Contacts`, (err, rows) => {
       callback(err, rows)
     })
   }
 
-  static create() {
-    // db.run(`INSERT INTO Contacts (name, company, telp_number, email) VALUES ('${req.body.name}', '${req.body.company}', '${req.body.telp_number}', '${req.body.email}')`, (err) => {
-    //   if(err) {
-    //     console.log(err);
-    //   }
-    // })
+  static create(input, callback) {
+    db.run(`INSERT INTO Contacts (name, company, telp_number, email) VALUES (
+      '${input.name}', '${input.company}', '${input.telp_number}', '${input.email}')`, (err) => {
+        callback(err)
+      })
+  }
+
+  static findByID(contactId, callback) {
+    db.get(`SELECT * FROM Contacts WHERE id = ${contactId}`, (err, rows) => {
+      callback(err, rows)
+    })
+  }
+
+  static update(data, contactId, callback) {
+    db.run(`UPDATE Contacts SET name = '${data.name}', company = '${data.company}',
+    telp_number = '${data.telp_number}', email = '${data.email}' WHERE id = ${contactId}`, (err) => {
+      callback(err)
+    })
+  }
+
+  static delete(contactId, callback) {
+    db.run(`DELETE FROM Contacts WHERE id = ${contactId}`, (err) => {
+      callback(err)
+    })
   }
 }
-
-// test
-Contact.findAll((err, rows) => {
-  if(err) {
-    console.log(err)
-  } else {
-    console.log(rows)
-  }
-})
 
 module.exports = Contact;
--- routers/contacts.js
+++ routers/contacts.js
@@ -6,23 +6,39 @@ const router = express.Router()
 // define the home page route
 router.get('/', function(req, res) {
   Contact.findAll((err, rows) => {
-    if(err) {
-      console.log(err)
-    } else {
-      console.log(rows)
-    }
+    res.render('contacts/index',{error: err, dataContacts: rows})
   })
 })
 
-// router.get('/add', (req, res) => {
-  // res.render('contacts/add')
-// })
+router.get('/add', function(req, res) {
+  res.render('contacts/add')
+})
 
-// router.post('/add', (req, res) => {
-  // Contacts.create(err => {
-    // res.send(req.body)
-    // res.redirect('/contacts')
-  // })
-// })
+router.post('/add', function(req, res) {
+  Contact.create(req.body, (err) => {
+    if(err) res.send(err)
+    res.redirect('/contacts')
+  })
+})
+
+router.get('/edit/:id', function(req, res) {
+  Contact.findByID(req.params.id, (err, rows) => {
+    res.render('contacts/edit', {error: err, dataContact: rows})
+  })
+})
+
+router.post('/edit/:id', function(req, res) {
+  Contact.update(req.body, req.params.id, (err) => {
+    if(err) res.send(err)
+    res.redirect('/contacts')
+  })
+})
+
+router.get('/delete/:id', function(req, res) {
+  Contact.delete(req.params.id, (err) => {
+    if(err) res.send(err)
+    res.redirect('/contacts')
+  })
+})
 
 module.exports = router

da1742edc34320c494588b88a5127677e8c3927c
alang <alangmahendra@gail.com>
2017-10-30 13:13:34 +0700

    delete profile

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ app.get('/',function(req,res) {
   res.send('masih rilis 0 menuju rilis 1')
 })
   //Contacts
-app.get('/Contacts',function(req,res){
+app.get('/contacts',function(req,res){
   db.all("SELECT * FROM Contacts", function(err, rows) {
         // console.log(rows);
         res.render('contacts',{rows})
         })
       })
 
-app.post('/Contacts',function(req,res) {
+app.post('/contacts',function(req,res) {
   db.all(`INSERT INTO Contacts(name,company,telp_number,email) VALUES("${req.body.name}",
   "${req.body.company}",
   "${req.body.telp_number}",
@@ -33,7 +33,7 @@ app.post('/Contacts',function(req,res) {
   })
 })
 
-app.get('/Contacts/delete/:id',function(req,res) {
+app.get('/contacts/delete/:id',function(req,res) {
   db.all(`DELETE FROM Contacts WHERE id = "${req.params.id}"`,function(err,rows) {
     res.redirect('/Contacts')
   })
@@ -41,59 +41,59 @@ app.get('/Contacts/delete/:id',function(req,res) {
 
 
 //Addresses
-app.get('/Addresses',function(req,res){
+app.get('/addresses',function(req,res){
   db.all("SELECT * FROM Addresses",function(err,rows) {
     res.render('addresses',{rows})
   })
 })
 
-app.post('/Addresses',function(req,res) {
+app.post('/addresses',function(req,res) {
   db.all(`INSERT INTO Addresses(street,city,zipcode) VALUES("${req.body.street}","${req.body.city}","${req.body.zipcode}")`,function(err,rows){
     res.redirect('/Addresses')
   })
 })
 
-app.get('/Addresses/delete/:id',function(req,res) {
+app.get('/addresses/delete/:id',function(req,res) {
   db.all(`DELETE FROM Addresses WHERE id = "${req.params.id}"`,function(err,rows) {
     res.redirect('/Addresses')
   })
 })
 
 //Groups
-app.get('/Groups',function(req,res){
+app.get('/groups',function(req,res){
   db.all("SELECT * FROM Groups",function(err,rows) {
       res.render('groups',{rows})
   })
 })
 
-app.post('/Groups',function(req,res){
+app.post('/groups',function(req,res){
   // console.log(req.body.groups);
   db.all(`INSERT INTO Groups(name_of_group) VALUES("${req.body.groups}")`,function(err,rows) {
       res.redirect('/Groups')
   })
 })
 
-app.get('/Groups/delete/:id',function(req,res) {
+app.get('/groups/delete/:id',function(req,res) {
   db.all(`DELETE FROM Groups WHERE id = "${req.params.id}"`,function(err,rows) {
     res.redirect('/Groups')
   })
 })
 
 //Profile
-app.get('/Profile',function(req,res){
+app.get('/profile',function(req,res){
   db.all("SELECT * FROM Profile",function(err,rows) {
       res.render('profile',{rows})
   })
 })
 
-app.post('/Profile',function(req,res) {
+app.post('/profile',function(req,res) {
   db.all(`INSERT INTO Profile(username,password) VALUES("${req.body.username}","${req.body.password}")`,function(err,rows){
     res.redirect('/Profile')
   })
 })
 
-app.get('/Profile/delete/:id',function(req,res) {
-  db.all(`DELETE FROM Addresses WHERE id = "${req.params.id}"`,function(err,rows) {
+app.get('/profile/delete/:id',function(req,res) {
+  db.all(`DELETE FROM Profile WHERE id = "${req.params.id}"`,function(err,rows) {
     res.redirect('/profile')
   })
 })

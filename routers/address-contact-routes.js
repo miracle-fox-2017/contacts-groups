3bdc0bf344fc7bca048b589347f7fafda4a8bbae
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-31 14:59:47 +0700

    Add address detail

--- routers/address-contact-routes.js
+++ routers/address-contact-routes.js
@@ -18,4 +18,32 @@ router.get('/', (req, res) => {
 	.catch(err => res.send(err));
 });
 
+router.get('/:id', (req, res) => {
+	let idContact = req.params.id;
+
+	let arrModel = [
+		ContactsModel.findById({id: idContact}),
+		AddressesModel.findAll()
+	]
+
+
+	Promise.all(arrModel).then((allModelData) => {
+		let allAddressData = allModelData[1];
+		let contactById = allModelData[0];
+
+
+		let arrAddress = [];
+
+		allAddressData.forEach( function(address, index) {
+			if (address.contacts_id == idContact) {
+				arrAddress.push(address);
+			}
+		});
+
+
+		res.render('address-with-contact', {contact: contactById, allAddress: arrAddress});
+	})
+	.catch(err => res.send(err));
+});
+
 module.exports = router;

c4d36c96caf71e439949f44a52c7912ff7c0c536
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-31 17:27:00 +0700

    All promise done, fix join2 dua table

--- routers/address-contact-routes.js
+++ routers/address-contact-routes.js
@@ -4,11 +4,18 @@ const ContactsModel = require('../models/contacts-model');
 const AddressesModel = require('../models/addresses-model');
 
 router.get('/', (req, res) => {
-	AddressesModel.findAll(function(err, allAddress) {
-		ContactsModel.findAll((err, allContacts) => {
-			res.render('addresses-contact', { data: allAddress, contacts: allContacts, joinedData: ContactsModel.findAllJoinContactsAddresses(allAddress, allContacts)});
-		});
-	});
+	let arrModel = [
+		AddressesModel.findAll(),
+		ContactsModel.findAll()
+	];
+
+	Promise.all(arrModel).then((allModelData) => {
+		let allAddressData = allModelData[0];
+		let allContactsData = allModelData[1];
+
+		 res.render('addresses-contact', { data: allAddressData, contacts: allContactsData, joinedData: ContactsModel.findAllJoinContactsAddresses(allAddressData, allContactsData)});
+	})
+	.catch(err => res.send(err));
 });
 
 module.exports = router;

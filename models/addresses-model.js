f3964beda4532e4c5efbfefdc7ac671d6c7ac395
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-31 17:20:04 +0700

    Address sudah promise

--- models/addresses-model.js
+++ models/addresses-model.js
@@ -3,106 +3,113 @@ const dbLocation = './database/database.db';
 const tableName = 'Addresses';
 
 class AddressesModel {
-	constructor() {
-
-	}
+	constructor() {}
 
 	/* Method pengganti addData(data) */
-	static create(data, callback) {
-		let db = new sqlite3.Database(dbLocation);
-		let sql = `INSERT INTO ${tableName} (street, city, zipcode, contacts_id) VALUES ("${data.street}", "${data.city}", ${+data.zipcode}, ${+data.contacts_id})`;
+	static create(data) {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbLocation);
+			let sql = `INSERT INTO ${tableName} (street, city, zipcode, contacts_id) VALUES ("${data.street}", "${data.city}", ${+data.zipcode}, ${+data.contacts_id})`;
 
-		db.run(sql, function(err, rows) {
-			if (err) {
-				console.error(err);
-				callback(err, null, null);
-			} else {
-				callback(null, rows, this.lastID);
-			}
+			db.run(sql, function(err, rows) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve({rows: rows, lastID: this.lastID});
+				}
+			});
+
+			db.close();
 		});
-
-		db.close();
 	}
 
 	/* Method pengganti getAllData(callback) */
-	static findAll(callback) {
-		let db = new sqlite3.Database(dbLocation);
-		let sql = `SELECT * FROM ${tableName}`;
-		db.all(sql, function(err, rows) {
-			if (err) {
-				console.error(err);
-				callback({err: err, message: 'Something wrong with sql query!'}, null);
-			} else {
-				callback(null, rows);
-			}
-		})
+	static findAll() {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbLocation);
+			let sql = `SELECT * FROM ${tableName}`;
+			db.all(sql, function(err, rows) {
+				if (err) {
+					reject({err: err, message: 'Something wrong with sql query!'});
+				} else {
+					resolve(rows);
+				}
+			})
 
-		db.close();
+			db.close();
+		});
 	}
 
-	/* Method pengganti getById(data, callback) */
-	static findById(data, callback) {
-		let db = new sqlite3.Database(dbLocation);
-		let sql = `SELECT * FROM ${tableName} WHERE id = ${data.id}`;
-		db.get(sql, (err, rows) =>{
-			if (err) {
-				console.error(err);
-				callback(err, null);
-			} else {
-				callback(null, rows);
-			}
-		});
+	/* Method pengganti getById(data) */
+	static findById(data) {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbLocation);
+			let sql = `SELECT * FROM ${tableName} WHERE id = ${data.id}`;
+			db.get(sql, (err, rows) =>{
+				if (err) {
+					reject(err);
+				} else {
+					resolve(rows);
+				}
+			});
 
-		db.close();
+			db.close();
+		});
 	}
 
 	/* Method pengganti getAllDataInnerJoin(tableSource, callback) */
-	static findAllDataInnerJoin(tableSource = 'Contacts', callback) {
-		let db = new sqlite3.Database(dbLocation);
-		let sql = `select ${tableName}.id, ${tableName}.street, ${tableName}.city, ${tableName}.zipcode , ${tableName}.contacts_id, ${tableSource}.name, ${tableSource}.company
-					from ${tableName}
-					inner join ${tableSource} ON ${tableName}.contacts_id =  ${tableSource}.id`;			
-		db.all(sql, function(err, rows) {
-			if (err) {
-				callback(err, null);
-				throw err;
-			} else {
-				callback(null, rows);
-			}
-		})
+	static findAllDataInnerJoin(tableSource = 'Contacts') {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbLocation);
+			let sql = `select ${tableName}.id, ${tableName}.street, ${tableName}.city, ${tableName}.zipcode , ${tableName}.contacts_id, ${tableSource}.name, ${tableSource}.company
+			from ${tableName}
+			inner join ${tableSource} ON ${tableName}.contacts_id =  ${tableSource}.id`;		
 
-		db.close();
+			db.all(sql, function(err, rows) {
+				if (err) {
+					reject(err);
+					
+				} else {
+					resolve(rows);
+				}
+			})
+
+			db.close();
+		});
 	}
 
 	/* Method pengganti updateDataById(data) */
-	static update(data, callback) {
-		let db = new sqlite3.Database(dbLocation);
-		let sql = `UPDATE ${tableName} SET street = "${data.editItem.street}", city = "${data.editItem.city}", zipcode = ${+data.editItem.zipcode}, contacts_id = ${+data.editItem.contacts_id} WHERE id = ${data.id}`;
-		db.run(sql, function(err, rows){
-			if (err) {
-				console.error(err);
-				callback(err, null, null);
-			} else {
-				callback(null, rows, this.lastID);
-			}
-		});
+	static update(data) {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbLocation);
+			let sql = `UPDATE ${tableName} SET street = "${data.editItem.street}", city = "${data.editItem.city}", zipcode = ${+data.editItem.zipcode}, contacts_id = ${+data.editItem.contacts_id} WHERE id = ${data.id}`;
+			db.run(sql, function(err, rows){
+				if (err) {
+					reject(err);
+				} else {
+					resolve({rows : rows, lastID:this.lastID});
+				}
+			});
 
-		db.close();
+			db.close();
+		});
 	}
 
 	/* Method pengganti deleteDataById(data) */
-	static removeItem(data, callback) {
-		let db = new sqlite3.Database(dbLocation);
-		let sql = `DELETE FROM ${tableName} WHERE id = ${data.id}`;
+	static removeItem(data) {
+		return new Promise((resolve, reject) => {
+			let db = new sqlite3.Database(dbLocation);
+			let sql = `DELETE FROM ${tableName} WHERE id = ${data.id}`;
 
-		db.run(sql, function(err, rows) {
-			if (err) {
-				callback(err, null, null);
-			} else {
-				callback(null, rows, this);
-			}
+			db.run(sql, function(err, rows) {
+				if (err) {
+					reject(err);
+				} else {
+					resolve();
+				}
+			});
+			db.close();
 		});
-		db.close();
 	}
 }
 
--- routers/address-routes.js
+++ routers/address-routes.js
@@ -4,52 +4,59 @@ const ContactsModel = require('../models/contacts-model');
 const AddressesModel = require('../models/addresses-model');
 
 router.get('/', (req, res) => {
-	AddressesModel.findAllDataInnerJoin('Contacts', function(err, allAddress) {
-
-		ContactsModel.findAll((err, allContacts) => {
-			res.render('addresses', { data: allAddress, contacts: allContacts });
-		});
-	});
+	AddressesModel.findAllDataInnerJoin('Contacts')
+		.then((allAddressData) => {
+			ContactsModel.findAll()
+				.then((allContactsData) => {
+					res.render('addresses', { data: allAddressData, contacts: allContactsData });
+				})
+				.catch(err => res.send(err));
+		})
+		.catch(err => res.send(err));
 });
 
 router.post('/', (req, res) => {
-	AddressesModel.create(req.body, function(err, allAddress, lastId) {
-		if (err == null) {
+	AddressesModel.create(req.body)
+		.then((allAddressData) => {
 			res.redirect('/addresses');
-		} else {
-			res.send(err);
-		}
-	});
+		})
+		.catch(err => res.send(err));
 });
 
 router.get('/edit/:id', (req, res) => {
-	AddressesModel.findAllDataInnerJoin('Contacts', function(err, allAddressJoined) {
-		ContactsModel.findAll((err, allContacts) => {
-			AddressesModel.findById({id: req.params.id}, (err, editedRows) =>{
-				res.render('addresses', { id: req.params.id, data: allAddressJoined, editItem: editedRows, contacts: allContacts});
+	let arrModel = [
+		AddressesModel.findAllDataInnerJoin('Contacts'), ContactsModel.findAll(),
+		AddressesModel.findById({id: req.params.id})
+	];
+
+	Promise.all(arrModel)
+		.then((allModelData) => {
+			let allJoinedContactAddress = allModelData[0];
+			let allContactsData = allModelData[1];
+			let editedAddressRow = allModelData[2];
+
+			res.render('addresses', { 
+				id: req.params.id, data: allJoinedContactAddress, 
+				editItem: editedAddressRow, contacts: allContactsData
 			});
-		});
-	});	
+		})
+		.catch(err => res.send(err));
 });
 
 router.post('/edit/:id', (req, res) => {
-	AddressesModel.update({id: req.params.id, editItem: req.body}, function(err, allAddress, lastId) {
-		if (err == null) {
+	AddressesModel.update({id: req.params.id, editItem: req.body})
+		.then((allAddressData) => {
 			res.redirect('/addresses/');
-		} else {
-			res.send(err);
-		}
-	});
+		})
+		.catch(err => res.send(err));
 });
 
 router.get('/delete/:id', (req, res) => {
-	AddressesModel.removeItem({id: req.params.id}, function(err, allAddress, lastId) {
-		if (err == null) {
+	AddressesModel.removeItem({id: req.params.id})
+		.then((allAddressData) => {
 			res.redirect('/addresses/');
-		} else {
-			res.send(err);
-		}
-	});
+		})
+		.catch(err => res.send(err));
 });
 
 module.exports = router;

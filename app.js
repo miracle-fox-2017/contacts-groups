627bb42631c55c0666cad8f5d632f37569a50ec8
fabiokounang <fabio_kosukeueki@yahoo.com>
2017-11-01 16:01:11 +0700

    RELEASE 1 DONE

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const app = express()
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database.db');
+
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.send('Hello World! This is my first website !')
+})
+
+app.get('/contacts', function (req, res) {
+  let queryContact = `SELECT * FROM Contacts`
+  db.all(queryContact, function(err, rowContacts){
+    res.render('contacts',{rowContacts})
+  })
+})
+
+app.get('/contacts/edit/:id', function (req, res) {
+  let getEdit = `SELECT * FROM Contacts WHERE id = ${req.params.id}`
+  db.all(getEdit, function(err, rowContacts) {
+    res.render('editContacts',{rowContacts})
+  })
+})
+
+app.post('/contacts/edit/:id', function (req, res) {
+  let id = req.params.id;
+  let name = req.body.name
+  let company = req.body.company;
+  let telepon = req.body.telp;
+  let email = req.body.email;
+
+  db.run(`UPDATE Contacts
+    SET name = '${name}', company = '${company}', telp = '${telepon}', email = '${email}' WHERE id = "${id}"`)
+    res.redirect('/contacts')
+})
+
+app.get('/contacts/delete/:id', function (req, res) {
+  db.run(`DELETE FROM Contacts
+   WHERE id = ${req.params.id}`);
+   res.redirect('/contacts')
+})
+
+app.post('/contacts',function (req, res) {
+  let name = req.body.name;
+  let company = req.body.company;
+  let telepon = req.body.telp;
+  let email = req.body.email;
+    db.run(`INSERT INTO Contacts
+      (name,company,telp,email)
+      VALUES('${name}','${company}','${telepon}','${email}')`);
+      res.redirect('/contacts')
+})
+
+// GROUPPP
+
+app.get('/groups', function (req, res) {
+  let queryGroup = `SELECT * FROM Groups`
+  db.all(queryGroup, function(err, rowGroups){
+    res.render('groups',{rowGroups})
+  })
+})
+
+app.post('/groups', function (req, res) {
+  let groupName = req.body.name_of_group;
+    db.run(`INSERT INTO Groups (name_of_group) VALUES('${groupName}')`);
+      res.redirect('/groups');
+})
+
+app.get('/groups/edit/:id', function (req, res) {
+  let getEdit = `SELECT * FROM Groups WHERE id = ${req.params.id}`
+  db.all(getEdit, function(err, rowGroups) {
+    res.render('editGroups',{rowGroups})
+  })
+})
+
+app.post('/groups/edit/:id', function (req, res) {
+  let id = req.params.id;
+  let nameGroup = req.body.name_of_group;
+  db.run(`UPDATE Groups
+    SET name_of_group = '${nameGroup}' WHERE id = "${id}"`)
+    res.redirect('/groups')
+})
+
+app.get('/groups/delete/:id', function (req, res) {
+  db.run(`DELETE FROM Groups
+   WHERE id = ${req.params.id}`);
+   res.redirect('/groups')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
---
+++ setup.js
@@ -0,0 +1,14 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database.db');
+
+db.serialize(function() {
+  db.run(`CREATE TABLE IF NOT EXISTS Contacts(id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(40), company VARCHAR(40), telp VARCHAR(15), email VARCHAR(40))`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS Groups(id INTEGER PRIMARY KEY AUTOINCREMENT, name_of_group VARCHAR(30))`)
+
+  db.run(`CREATE TABLE IF NOT EXISTS Profile(id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(50), password VARCHAR(20))`)
+
+  db.run(`CREATE TABLE IF NOT EXISTS Addresses(id INTEGER PRIMARY KEY AUTOINCREMENT, street VARCHAR(40), city VARCHAR(20), zipcode INTEGER)`)
+});
+
+db.close();

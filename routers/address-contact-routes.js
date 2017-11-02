724cf24e111d8e4ae9355ca5d6bd8750935440a6
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-02 13:54:29 +0700

    Revisi address with contact dengan cara foreach where

--- routers/address-contact-routes.js
+++ routers/address-contact-routes.js
@@ -4,18 +4,52 @@ const ContactsModel = require('../models/contacts-model');
 const AddressesModel = require('../models/addresses-model');
 
 router.get('/', (req, res) => {
-	let arrModel = [
-		AddressesModel.findAll(),
-		ContactsModel.findAll()
-	];
+	// let arrModel = [
+	// 	AddressesModel.findAll(),
+	// 	ContactsModel.findAll()
+	// ];
 
-	Promise.all(arrModel).then((allModelData) => {
-		let allAddressData = allModelData[0];
-		let allContactsData = allModelData[1];
+	// Promise.all(arrModel).then((allModelData) => {
+	// 	let allAddressData = allModelData[0];
+	// 	let allContactsData = allModelData[1];
+	// 	console.log(ContactsModel.findAllJoinContactsAddresses(allAddressData, allContactsData))
+	// 	 res.render('addresses-contact', { 
+	// 	 	data: allAddressData, contacts: allContactsData, 
+	// 	 	joinedData: ContactsModel.findAllJoinContactsAddresses(allAddressData, allContactsData)
+	// 	 });
+	// })
+	// .catch(err => res.send(err));
 
-		 res.render('addresses-contact', { data: allAddressData, contacts: allContactsData, joinedData: ContactsModel.findAllJoinContactsAddresses(allAddressData, allContactsData)});
-	})
-	.catch(err => res.send(err));
+	AddressesModel.findAll().
+		then((allAddressData) => {
+
+			ContactsModel.findAll().
+				then((allContactsData) => {
+					allAddressData.forEach((address, index) => {
+						ContactsModel.findById({id : address.contacts_id})
+						.then((foundContact) => {
+							if (address.contacts_id == foundContact.id) {
+								address.name = foundContact.name;
+								address.company = foundContact.company;
+								address.telp_number = foundContact.telp_number;
+								address.email = foundContact.email;
+							}
+
+							if (index == allAddressData.length - 1) {
+								
+								res.render('addresses-contact', { 
+									data: allAddressData, contacts: allContactsData, 
+									joinedData: allAddressData
+								});
+							}
+						})
+						.catch(err => res.send(err));
+					})
+				})
+				.catch(err => res.send(err));
+				
+		})
+		.catch(err => res.send(err));
 });
 
 router.get('/:id', (req, res) => {
